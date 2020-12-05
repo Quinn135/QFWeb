e9db755d80ac7a3b7ed8483f88720de91c660eaf
Quinn Cooperstein <quinn.flynn.coop@gmail.com>
2020-12-05 10:49:13 -0600

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,37 @@
+
+<!doctype html>
+
+<html lang="en">
+<head><meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    
+    <link rel="icon" href="/favicon.png">
+    
+
+<link rel="stylesheet" href="https://unpkg.com/purecss@2.0.3/build/pure-min.css" integrity="sha384-cg6SkqEOCV1NbJoCu11+bm0NvBRc8IYLRGXkmNrqUBfTjmMYwNKPWBTIKyw9mHNJ" crossorigin="anonymous">
+        
+<head>
+    <title>QF Web</title>
+<meta name = "keywords" content = "QFWeb, QF Web, Q F Web" />
+      <meta name = "description" content = "This is a test website." />
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+    
+<body>
+    
+    <p style="text-align: center;"><span style="font-family: Impact, Charcoal, sans-serif; font-size: 60px;">This is a test, I repeat, a test.</span></p>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</body>
